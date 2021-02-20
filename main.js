5ffe69e80b8de6b100522d88f7e08ae7b4dc60dc
Joy Harjanto <38966343+joyharjanto@users.noreply.github.com>
2021-02-20 17:04:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,106 +5,88 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
-class Field {
-  constructor(field = [[]]) {
-    this.field = field;
-    this.locationX = 0;
-    this.locationY = 0;
-    // Set the "home" position before the game starts
-    this.field[0][0] = pathCharacter;
+class Field { 
+  constructor(grid = [[]]){
+    this.grid = grid;
+    this.index = [0,0];
+  }
+  
+  runGame(){
+    this.play = true;
+    while(this.play){
+    this.print();
+    this.askQuestionMove();
+    this.checkBounds();
+    this.checkHole();
+    this.checkWin();
+    this.replaceBlock();
+  }
   }
 
-  runGame() {
-    let playing = true;
-    while (playing) {
-      this.print();
-      this.askQuestion();
-      if (!this.isInBounds()) {
-        console.log('Out of bounds instruction!');
-        playing = false;
-        break;
-      } else if (this.isHole()) {
-        console.log('Sorry, you fell down a hole!');
-        playing = false;
-        break;
-      } else if (this.isHat()) {
-        console.log('Congrats, you found your hat!');
-        playing = false;
-        break;
-      }
-      // Update the current location on the map
-      this.field[this.locationY][this.locationX] = pathCharacter;
-    }
-  }
-
-  askQuestion() {
-    const answer = prompt('Which way? ').toUpperCase();
-    switch (answer) {
-      case 'U':
-        this.locationY -= 1;
-        break;
-      case 'D':
-        this.locationY += 1;
-        break;
-      case 'L':
-        this.locationX -= 1;
-        break;
-      case 'R':
-        this.locationX += 1;
-        break;
+  askQuestionMove(){
+    const input = prompt('Which way? ');
+    switch(input){
+      case "L":
+      this.index[1] = this.index[1] - 1;
+      break;
+      case "R":
+      this.index[1] = this.index[1] + 1;
+      break;
+      case "U":
+      this.index[0] = this.index[0] - 1;
+      break;
+      case "D":
+      this.index[0] = this.index[0] + 1;
+      break;
       default:
-        console.log('Enter U, D, L or R.');
-        this.askQuestion();
-        break;
+      console.log("Please enter something else")
+      break;
+    }
+    }
+
+
+  
+  checkBounds(){
+    if(this.index[0] < 0 || this.index[0] >= this.grid.length || this.index[1] < 0 || this.index[1] >= this.grid[0].length){
+      console.log("out of bounds");
+      this.play = false;
+      return this.play;
+    }
+  }
+  
+  checkHole(){
+    if(this.grid[this.index[0]][this.index[1]] === 'O'){
+      console.log("game over bc you fell into a hoole");
+      this.play = false;
+      return this.play; 
     }
   }
 
-  isInBounds() {
-    return (
-      this.locationY >= 0 &&
-      this.locationX >= 0 &&
-      this.locationY < this.field.length &&
-      this.locationX < this.field[0].length
-    );
+  checkWin(){
+    if(this.grid[this.index[0]][this.index[1]] === '^'){
+      console.log("win");
+      this.play = false;
+    }  
   }
 
-  isHat() {
-    return this.field[this.locationY][this.locationX] === hat;
+  replaceBlock(){
+    this.grid[this.index[0]][this.index[1]] = '*';
   }
 
-  isHole() {
-    return this.field[this.locationY][this.locationX] === hole;
-  }
-
-  print() {
-    const displayString = this.field.map(row => {
-        return row.join('');
-      }).join('\n');
-    console.log(displayString);
-  }
-
-  static generateField(height, width, percentage = 0.1) {
-    const field = new Array(height).fill(0).map(el => new Array(width));
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const prob = Math.random();
-        field[y][x] = prob > percentage ? fieldCharacter : hole;
-      }
-    }
-    // Set the "hat" location
-    const hatLocation = {
-      x: Math.floor(Math.random() * width),
-      y: Math.floor(Math.random() * height)
-    };
-    // Make sure the "hat" is not at the starting point
-    while (hatLocation.x === 0 && hatLocation.y === 0) {
-      hatLocation.x = Math.floor(Math.random() * width);
-      hatLocation.y = Math.floor(Math.random() * height);
-    }
-    field[hatLocation.y][hatLocation.x] = hat;
-    return field;
+  print(){
+    for (let i = 0; i < this.grid.length; i++) {
+      console.log(this.grid[i].join(""));
+    }     
   }
 }
 
-const myfield = new Field(Field.generateField(10, 10, 0.2));
-myfield.runGame();
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+
+myField.runGame()
+
+
